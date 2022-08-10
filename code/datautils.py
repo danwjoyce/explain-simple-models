476d7349e9782e2dbf0778dd903668ba5a61f782
Dan Joyce <danjoyce@doctors.org.uk>
2022-08-10 13:33:31 +0100

    cleaned repo for TIFU paper submission

--- code/datautils.py
+++ code/datautils.py
@@ -16,7 +16,7 @@ class GenDataset( Dataset ):
       df = df_csv.loc[ df_csv["train"] == 0, : ]
     
     x = df.loc[:, pred_vars ].values   # predictor variables 
-    y = df.loc[:, target_var].values     # target
+    y = df.loc[:, target_var].values   # target
 
     self.x = torch.tensor( x, dtype = torch.float32 )
     self.y = torch.tensor( y, dtype = torch.float32 )
